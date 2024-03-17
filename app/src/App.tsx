444a7473633c71e5dd46576bba9aefa3c80c67a8
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-03-17 16:19:36 +0000

    save 17-03

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,24 +6,26 @@ function App() {
   return (
     <div className="container">
       <div className="login">
-        <img src={Logo} alt="Logo da empresa" id='logo' />
-        <h2>Bem-vindo(a) ao Stock Pro</h2>
-        <h1>Login</h1>
+        <div className="card">
+          <img src={Logo} alt="Logo da empresa" id='logo' className='logo' />
+          <h2>Bem-vindo(a) ao Stock Pro</h2>
+          <h1>Login</h1>
 
-        <div className='forms'>
+          <div className='forms'>
 
-          <input type="email" required placeholder='Email' id='email' />
+            <input type="email" required placeholder='Email' id='email' />
 
-          <input type="password" id="password" placeholder='Senha' />
+            <input type="password" id="password" placeholder='Senha' />
+          </div>
+
+          <div className="logar">
+            <a id='esqueciSenha' href="#">Esqueci minha senha </a>
+            <a id='criarConta' href='#'>Criar conta</a>
+          </div>
+
+
+          <button type='submit'>Login</button>
         </div>
-
-        <div className="logar">
-          <a id='esqueciSenha' href="#">Esqueci minha senha </a>
-          <a id='criarConta' href='#'>Criar conta</a>
-        </div>
-
-
-        <button type='submit'>Login</button>
       </div>
 
       <div className="image">
